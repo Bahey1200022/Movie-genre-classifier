c8e445e6e3e4e2c43620d32a88ad208a8e4f3758
Bahey1200022 <bahy2002@gmail.com>
2025-11-22 23:37:19 +0100

    preprocessing

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,21 @@
 import pandas as pd
+import ast
 csv="final.csv"
 df=pd.read_csv("output.csv")
 ids=pd.read_csv("ids.csv")
-import ast
-for i,row in enumerate(df.iterrows()):
+finaldf=pd.DataFrame(columns=['path','genres'])
+for i, row in df.iterrows():
     genres_list = df.iloc[i, 1]
-    genre_list = ast.literal_eval(genres_list)  # convert string to list
+    genre_list = ast.literal_eval(genres_list)
+    filmlist=list()
     for genre in genre_list:
-        print(ids.iloc[:,0][genre])
+        genre_id = ids.loc[ids['genre'] == genre, 'id'].values[0]
+        filmlist.append(genre_id)
+        
+    finaldf.loc[i]=[row[0],filmlist]
+    
+finaldf.to_csv(csv)   
+
     
     
 
